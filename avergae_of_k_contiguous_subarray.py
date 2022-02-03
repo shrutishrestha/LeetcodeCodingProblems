6879f21f7b6d3dfdc7fe37b1dbc2e88ea455e7bd
shruti <shruti.shresthaa@gmail.com>
2022-02-02 22:31:23 -0500

    sumofsubarray

---
+++ avergae_of_k_contiguous_subarray.py
@@ -0,0 +1,14 @@
+# Given an array, find the average of all subarrays of ‘K’ contiguous elements in it.
+
+def func(k, arr):
+    average_list = []
+    average = 0
+    start = 0
+    for i in range(len(arr)):
+        average += arr[i]
+        if i>=k-1:
+            average_list.append(average/k)
+            average = average - arr[start]
+            start += 1
+    return average_list
+print(func(5, [1, 3, 2, 6, -1, 4, 1, 8, 2]))
